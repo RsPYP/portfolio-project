e76afe87a2514a0c786ea3a057d38e238d958616
RsPYP <hirata.mizuki.970410@google.com>
2024-08-28 01:13:50 +0900

    react導入から再構築

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from "react";
-import { Container, CssBaseline, Box } from "@mui/material";
-import Navbar from "./components/Navbar";
-import Profile from "./components/Profile";
-import Projects from "./components/Projects";
-import Blog from "./components/Blog";
-import Footer from "./components/Footer";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <CssBaseline />
-      <Navbar />
-      <Container maxWidth="lg">
-        <Box my={4}>
-          <Profile />
-          <Projects />
-          <Blog />
-        </Box>
-      </Container>
-      <Footer />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
